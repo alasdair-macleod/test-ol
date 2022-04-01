638adc30a4c3de385ab69769b5134fde029f6aee
alasdair-macleod <alithedrummer@gmail.com>
2022-04-01 11:38:49 +0100

    Update gml-test.ts
    removing unnecessary extra code. should still reproduce bug like this.

--- src/app/gml-test.ts
+++ src/app/gml-test.ts
@@ -1,9 +1,5 @@
 import GML2 from 'ol/format/GML2';
 
 export class TestClass {
-  private gml2 = new GML2();
-
-  constructor() {
-     this.gml2.readFeatures(undefined);
-  }
+  constructor() {}
 }
