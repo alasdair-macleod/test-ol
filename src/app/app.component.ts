7b137acd8fa8cf69876519b29fdf4e49c3e6d033
amacleo7 <amacleo7@ed.ac.uk>
2022-04-01 09:44:43 +0100

    minimal reproduction of GML type bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestClass } from './gml-test'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-ol';
+  test = new TestClass()
 }
